9614a3d16cfd9a98bd63d1bc6c16c2d6379208c6
Eduardo Donato <eduardo.donato@gmail.com>
2021-08-07 13:08:31 -0300

    First Version

--- README.md
+++ README.md
@@ -1,23 +1,38 @@
 # NPX CARD
+
 This my NPX card unique style to connect with me directly via console or terminal
 
-👇 just hit 
+## 👇 just hit
+
+If you have Node.js installed:
+
 ```bash
-npx anmol
+npx ebdonato
 ```
-And get to know me in unique style.
 
-### I spent a non-trivial amount of effort building and designing this iteration of npx card, and I am proud of it! All I ask of you all is to put a **star** ⭐ on this project and not claim this effort as your own ♥.
+Or a Docker alternative:
 
-### SCREENSHOT
+```bash
+docker run -it --rm --name ebdonato-business-card -v ${PWD}:/usr/src/app -w /usr/src/app node:14 npx ebdonato
+```
+
+And get to know me in some kind of an unique style.
+
+## Thanks [Anmol Pratap Singh (anmol098)](https://github.com/anmol098)
+
+_anmol098_ spent a non-trivial amount of effort building and designing his iteration of [npx card](https://github.com/anmol098/npx_card), and he is proud of it! All he asks of you all is to put a **star** ⭐ on this [project](https://github.com/anmol098/npx_card) and not claim this effort as your own ♥.
+
+**I forked npx business card my from his**
+
+## SCREENSHOT
 
 The final output might look something like this:
 
-![image](https://github.com/anmol098/npx_card/blob/master/demo.gif)
-
+![image](demo.png)
 
 <hr/>
 
-##### STEPS TO CREATE YOUR OWN
-The article written by our friend @jackboberg. I used the same for the reference to deploy the package. 
-[Write a Simple npx Business Card](https://studioelsa.se/blog/open-source-oss-npx-business-card). 
+## STEPS TO CREATE YOUR OWN
+
+The article written by @jackboberg. I used the same for the reference to deploy the package.
+[Write a Simple npx Business Card](https://studioelsa.se/blog/open-source-oss-npx-business-card).
--- card.js
+++ card.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 
-'use strict'
+"use strict"
 
-const boxen = require("boxen");
-const chalk = require("chalk");
-const inquirer = require("inquirer");
-const clear = require("clear");
-const open = require("open");
-const fs = require('fs');
-const request = require('request');
-const path = require('path');
-const ora = require('ora');
-const cliSpinners = require('cli-spinners');
-clear();
+const boxen = require("boxen")
+const chalk = require("chalk")
+const inquirer = require("inquirer")
+const clear = require("clear")
+const open = require("open")
+const qrCode = require("qrcode-terminal")
 
-const prompt = inquirer.createPromptModule();
+clear()
+
+const prompt = inquirer.createPromptModule()
 
 const questions = [
     {
@@ -23,99 +20,73 @@ const questions = [
         message: "What you want to do?",
         choices: [
             {
-                name: `Send me an ${chalk.green.bold("email")}?`,
-                value: () => {
-                    open("mailto:hi@anmolsingh.me");
-                    console.log("\nDone, see you soon at inbox.\n");
-                }
-            },
-            {
-                name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
-                value: () => {
-                    // cliSpinners.dots;
-                    const loader = ora({
-                        text: ' Downloading Resume',
-                        spinner: cliSpinners.material,
-                    }).start();
-                    let pipe = request('https://anmolsingh.me/api/resume').pipe(fs.createWriteStream('./anmol-resume.html'));
-                    pipe.on("finish", function () {
-                        let downloadPath = path.join(process.cwd(), 'anmol-resume.html')
-                        console.log(`\nResume Downloaded at ${downloadPath} \n`);
-                        open(downloadPath)
-                        loader.stop();
-                    });
-                }
-            },
-            {
                 name: "Just quit.",
                 value: () => {
-                    console.log("Hasta la vista.\n");
-                }
-            }
-        ]
-    }
-];
+                    console.log("Hasta la vista.\n")
+                },
+            },
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:eduardo.donato@gmail.com")
+                    console.log("Done, see you soon at inbox.\n")
+                },
+            },
+        ],
+    },
+]
 
 const data = {
-    name: chalk.bold.green("             Anmol Pratap Singh"),
-    handle: chalk.white("@anmol098"),
-    work: `${chalk.white("Software Engineer at")} ${chalk
-        .hex("#2b82b2")
-        .bold("ClearTax")}`,
-    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("misteranmol"),
-    github: chalk.gray("https://github.com/") + chalk.green("anmol098"),
-    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("anmol098"),
-    web: chalk.cyan("https://anmolsingh.me"),
-    npx: chalk.red("npx") + " " + chalk.white("anmol"),
+    name: chalk.bold.green("              Eduardo Batista Donato"),
+    handle: chalk.white("@ebdonato"),
+    work: `${chalk.white("              Software Developer at")} ${chalk.hex("#2b82b2").bold("Eletromarquez")}`,
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("ebdonato"),
+    github: chalk.gray("https://github.com/") + chalk.green("ebdonato"),
+    linkedIn: chalk.gray("https://linkedin.com/in/") + chalk.blue("ebdonato"),
+    web: chalk.gray("https://gravatar.com/") + chalk.redBright("ebdonato"),
+    npx: chalk.red("npx") + " " + chalk.white("ebdonato"),
 
-    labelWork: chalk.white.bold("       Work:"),
     labelTwitter: chalk.white.bold("    Twitter:"),
     labelGitHub: chalk.white.bold("     GitHub:"),
     labelLinkedIn: chalk.white.bold("   LinkedIn:"),
     labelWeb: chalk.white.bold("        Web:"),
-    labelCard: chalk.white.bold("       Card:")
-};
+    labelCard: chalk.white.bold("       Card:"),
+}
 
-const me = boxen(
-    [
-        `${data.name}`,
-        ``,
-        `${data.labelWork}  ${data.work}`,
-        ``,
-        `${data.labelTwitter}  ${data.twitter}`,
-        `${data.labelGitHub}  ${data.github}`,
-        `${data.labelLinkedIn}  ${data.linkedin}`,
-        `${data.labelWeb}  ${data.web}`,
-        ``,
-        `${data.labelCard}  ${data.npx}`,
-        ``,
-        `${chalk.italic(
-            "I am currently looking for new opportunities,"
-        )}`,
-        `${chalk.italic("my inbox is always open. Whether you have a")}`,
-        `${chalk.italic(
-            "question or just want to say hi, I will try "
-        )}`,
-        `${chalk.italic(
-            "my best to get back to you!"
-        )}`
-    ].join("\n"),
-    {
-        margin: 1,
-        float: 'center',
-        padding: 1,
-        borderStyle: "single",
-        borderColor: "green"
-    }
-);
+qrCode.generate("https://github.com/ebdonato", {small: true}, function (code) {
+    const arr = code.split("\n")
 
-console.log(me);
-const tip = [
-    `Tip: Try ${chalk.cyanBright.bold(
-        "cmd/ctrl + click"
-    )} on the links above`,
-    '',
-].join("\n");
-console.log(tip);
+    let i = 0
 
-prompt(questions).then(answer => answer.action());
+    const me = boxen(
+        [
+            `${arr[i++]} ${data.name}`,
+            `${arr[i++]} ${data.work}`,
+            `${arr[i++]}`,
+            `${arr[i++]} ${data.labelTwitter}  ${data.twitter}`,
+            `${arr[i++]} ${data.labelGitHub}  ${data.github}`,
+            `${arr[i++]} ${data.labelLinkedIn}  ${data.linkedIn}`,
+            `${arr[i++]} ${data.labelWeb}  ${data.web}`,
+            `${arr[i++]}`,
+            `${arr[i++]} ${data.labelCard}  ${data.npx}`,
+            `${arr[i++]}`,
+            `${arr[i++]}     ${chalk.italic("I am an electrical and planning engineer.")}`,
+            `${arr[i++]}     ${chalk.italic("I am currently a Microsoft Power Platform designer,")}`,
+            `${arr[i++]}     ${chalk.italic("(Power BI, Power Apps e Power Automate),")}`,
+            `${arr[i++]}     ${chalk.italic("and modern web full-stack developer (VueJS e ExpressJS).")}`,
+        ].join("\n"),
+        {
+            margin: 1,
+            float: "center",
+            padding: 1,
+            borderStyle: "round",
+            borderColor: "green",
+        }
+    )
+
+    console.log(me)
+    const tip = [`Tip: Try ${chalk.cyanBright.bold("cmd/ctrl + click")} on the links above`, ""].join("\n")
+    console.log(tip)
+
+    prompt(questions).then((answer) => answer.action())
+})
